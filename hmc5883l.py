e96fe625a41249a974d44d5660ad1b2f2cdaffc5
BG6WRI <bg6wri@gmail.com>
2018-03-11 00:13:27 +0800

    HMC5883L
     HMC5883L

--- hmc5883l.py
+++ hmc5883l.py
@@ -1,13 +1,74 @@
 #!/usr/bin/python  
 import wiringpi as wpi
-
+import math
+import time
 a=wpi.wiringPiI2CSetup(0x1e)
-while True:
-    wpi.wiringPiI2CWriteReg8(a,0x02,0b10000000)
-   # t=10000
-    #while t>0:
-     #   t=t-1
-    print wpi.wiringPiI2CReadReg8(a,0x04)
+
+
+	#set range
+wpi.wiringPiI2CWriteReg8(a,0x01, 0b001 << 5);
+mgPerDigit = float(0.92);
+
+	#setMeasurementMode
+
+
+value = wpi.wiringPiI2CReadReg8(a,0x02);
+value &= 0b11111100;
+value |= 0b00;	
+
+wpi.wiringPiI2CWriteReg8(a,0x02, value);
+
+
+	#setDataRate
+
+
+value = wpi.wiringPiI2CReadReg8(a,0x00);
+value &= 0b11100011;
+value |= (0b101 << 2);	
+
+wpi.wiringPiI2CWriteReg8(a,0x00, value);
+
+
+	#setSamples
+
+value = wpi.wiringPiI2CReadReg8(a,0x00);
+value &= 0b10011111;
+value |= (0b11 << 5);
+
+wpi.wiringPiI2CWriteReg8(a,0x00, value);
+
+
+	#setOffset
+xOffset = float(141);
+yOffset = float(167);
 
 
 
+	#compass
+while True
+		#Vector norm = compass.readNormalize();
+	XAxis = (float(wpi.wiringPiI2CReadReg16(a,0x03)) - xOffset) * mgPerDigit;
+	YAxis = (float(wpi.wiringPiI2CReadReg16(a,0x07)) - yOffset) * mgPerDigit;
+	ZAxis = float(wpi.wiringPiI2CReadReg16(a,0x05)) * mgPerDigit;
+
+
+
+	heading = math.atan2(YAxis, XAxis);
+
+	declinationAngle = (4.0 + (26.0 / 60.0)) / (180 / math.pi);
+
+	heading = heading + declinationAngle;
+	  	
+	if (heading < 0):
+		heading += 2 * math.pi;
+
+	if (heading > 2 * math.pi):
+		heading -= 2 * math.pi;
+
+	headingDegrees = heading * 180/math.pi;
+
+	print " Degress = "
+	print headingDegrees
+
+
+	time.delay(100)
