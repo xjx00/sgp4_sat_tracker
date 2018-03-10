b63bac0e3a89fb72ff0c4a503d9bad658236cd7c
BG6WRI <bg6wri@gmail.com>
2018-03-11 00:16:22 +0800

    HMC5883L
    HMC5883L

--- hmc5883l.py
+++ hmc5883l.py
@@ -1,7 +1,10 @@
-#!/usr/bin/python  
+#!/usr/bin/python
+
 import wiringpi as wpi
 import math
 import time
+
+
 a=wpi.wiringPiI2CSetup(0x1e)
 
 
@@ -9,8 +12,8 @@ a=wpi.wiringPiI2CSetup(0x1e)
 wpi.wiringPiI2CWriteReg8(a,0x01, 0b001 << 5);
 mgPerDigit = float(0.92);
 
-	#setMeasurementMode
 
+	#setMeasurementMode
 
 value = wpi.wiringPiI2CReadReg8(a,0x02);
 value &= 0b11111100;
@@ -20,7 +23,6 @@ wpi.wiringPiI2CWriteReg8(a,0x02, value);
 
 
 	#setDataRate
-
 
 value = wpi.wiringPiI2CReadReg8(a,0x00);
 value &= 0b11100011;
@@ -39,33 +41,34 @@ wpi.wiringPiI2CWriteReg8(a,0x00, value);
 
 
 	#setOffset
+
 xOffset = float(141);
 yOffset = float(167);
 
 
-
 	#compass
+
 while True
-		#Vector norm = compass.readNormalize();
+
 	XAxis = (float(wpi.wiringPiI2CReadReg16(a,0x03)) - xOffset) * mgPerDigit;
 	YAxis = (float(wpi.wiringPiI2CReadReg16(a,0x07)) - yOffset) * mgPerDigit;
 	ZAxis = float(wpi.wiringPiI2CReadReg16(a,0x05)) * mgPerDigit;
 
 
-
 	heading = math.atan2(YAxis, XAxis);
-
 	declinationAngle = (4.0 + (26.0 / 60.0)) / (180 / math.pi);
-
 	heading = heading + declinationAngle;
-	  	
+
+
 	if (heading < 0):
 		heading += 2 * math.pi;
 
 	if (heading > 2 * math.pi):
 		heading -= 2 * math.pi;
 
+
 	headingDegrees = heading * 180/math.pi;
+
 
 	print " Degress = "
 	print headingDegrees
