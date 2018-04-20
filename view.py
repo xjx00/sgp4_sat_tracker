817684ff8339f6606ff6ce45ea7a1b6e7e765ffc
BG6WRI <bg6wri@gmail.com>
2018-04-20 22:36:32 +0800

    #
    #

--- view.py
+++ view.py
@@ -8,25 +8,7 @@ import serial
 import time
 import GetLook
 import math
-
-
-from sgp4.earth_gravity import wgs72
-from sgp4.ext import invjday, newtonnu, rv2coe
-from sgp4.io import twoline2rv
-from sgp4.propagation import sgp4
-
-
-F                        =        1.0 / 298.26
-XKMPER_WGS72             =        6378.135
-EPOCH_JAN1_12H_2000      =        2451545.0
-SEC_PER_DAY              =        86400.0
-OMEGA_E                  =        1.00273790934
-
-
-class Eci(object):
-	position=[]
-	velocity=[]
-
+import sgp4
 
 
 ser=serial.Serial("/dev/ttyUSB0",115200,timeout=0.5)
@@ -41,19 +23,29 @@ compass.init()
 
 
 
-line1 = ('1 00005U 58002B   00179.78495062  '
-          '.00000023  00000-0  28098-4 0  4753')
-line2 = ('2 00005  34.2682 348.7242 1859667 '
-          '331.7664  19.3264 10.82419157413667')
+#time.localtime(time.time())
+t#ime.time()
+#date = satellite.jdsatepoch
 
-satellite = twoline2rv(line1, line2, wgs72)
 
-eciSat  = Eci()
-Site    = Eci()
-#eciSite = Eci()
+while True:
 
-Site.Position =[ deg2rad(30.463) , deg2rad(114.43) , 0.05 ]
-#  Site.Position =[ deg2rad(Lat),deg2rad(Lon),kmAlt ]
+
+	date_now = sgp4.fklghdksfhg
+
+	eciSat = sgp4.get_eciSat(date_now)
+
+	#eciSat.Position = list(P)
+	#eciSat.Velocity = list(V)
+
+	GetLook.GetLook(date_now,eciSat)
+	#date_now为Julian形式
+
+	AZ_now = accel.read()     #azimuth
+	EL_now = compass.read()   #elevation
+
+
+	time.sleep(0.1)
 
 
 
@@ -85,23 +77,3 @@ Site.Position =[ deg2rad(30.463) , deg2rad(114.43) , 0.05 ]
 #print( c + " 的ASCII 码为", ord(c))
 #print( a , " 对应的字符为", chr(a))
 
-
-
-time.localtime(time.time())
-time.time()
-date = satellite.jdsatepoch
-
-
-while True:
-	P,V = satellite.propagate(2000, 6, 28, 0, 50, 19.733567)
-	eciSat.Position = list(P)
-	eciSat.Velocity = list(V)
-
-	GetLook.GetLook(date_now,eciSat,Site)
-	#date_now为Julian形式
-
-	AZ_now = accel.read()     #azimuth
-	EL_now = compass.read()   #elevation
-
-
-	time.sleep(0.1)
