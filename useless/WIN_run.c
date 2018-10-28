4588c19b3a7d6b4e0a957aa526e5b9ce7896c32c
BG6WRI <bg6wri@gmail.com>
2018-10-28 20:43:11 +0800

    #
    #

---
+++ useless/WIN_run.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h> 
+int main()
+{
+	system("start python wri_sattrack.py"); //将这个cmd替换为在CMD下可运行的命令也可以，例如pause，注意一定要加括号
+    system('pause');
+}
