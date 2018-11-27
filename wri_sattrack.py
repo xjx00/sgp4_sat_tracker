b2319f762b09e13a82629d3bb3af52a0ffd846d1
BG6WRI <bg6wri@gmail.com>
2018-11-27 11:03:06 +0800

    shell_serial_out

--- wri_sattrack.py
+++ wri_sattrack.py
@@ -3,17 +3,23 @@
 
 import time
 import jdcal
+import sys
 import serial
 import GetUserData
 import GetSat
 import GetLook
 
 
-
+'''
 #if you want to use serial.
-#ser = serial.Serial('/dev/ttyUSB0',9600)
+port_num=raw_input("Please Enter Your Serial Port Number:\n")
 
-	
+if sys.platform == "linux2":
+	ser=serial.Serial("COM"+porn_num,9600,timeout=0.5)
+if sys.platform == "win32":
+	ser=serial.Serial("/dev/ttyUSB"+port_num,9600,timeout=0.5)
+'''
+
 
 GetUserData.update("shell")
 
@@ -33,9 +39,11 @@ while True:
 	AZ,EL = GetLook.GetLook(date_now_julian,eciSat)
 
 	print AZ,EL
-	
+'''	
 	#if you want to use serial
-	#ser.write("Whatever you need")
+	ser.write("Whatever you need")
+	#For example:   ser.write(AZ,EL)
 
 	#If you need to control the frequency of date out:
-	#time.sleep()                 #Second
+	time.sleep()                 #Second
+'''
